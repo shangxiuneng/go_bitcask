f16ffded39dbd30ce9baaf3f065191066d192c67
shangxiuneng <shangxiuneng95@163.com>
2024-09-26 10:01:51 +0800

    feat: 新增art树索引

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/google/btree v1.1.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/plar/go-adaptive-radix-tree v1.0.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
--- index/art.go
+++ index/art.go
@@ -1,1 +1,58 @@
 package index
+
+import (
+	"errors"
+	art "github.com/plar/go-adaptive-radix-tree"
+	"go_bitcask/data"
+	"sync"
+)
+
+// ArtTree 自适应基数树
+type ArtTree struct {
+	Index
+
+	lock    *sync.Mutex
+	artTree art.Tree
+}
+
+func newArtTree() Index {
+	return &ArtTree{
+		artTree: art.New(),
+		lock:    new(sync.Mutex),
+	}
+}
+
+func (a *ArtTree) Put(key []byte, record *data.RecordPos) error {
+	a.artTree.Insert(key, record)
+	return nil
+}
+func (a *ArtTree) Get(key []byte) (*data.RecordPos, error) {
+	if len(key) == 0 {
+		return nil, errors.New("key is nil")
+	}
+	value, found := a.artTree.Search(key)
+	if !found {
+		return nil, nil
+	}
+
+	return value.(*data.RecordPos), nil
+}
+
+func (a *ArtTree) Delete(key []byte) error {
+	if len(key) == 0 {
+		return errors.New("key is nil")
+	}
+	a.lock.Lock()
+	defer a.lock.Unlock()
+
+	a.artTree.Delete(key)
+	return nil
+}
+
+func (a *ArtTree) Iterator(reverse bool) Iterator {
+	return nil
+}
+
+// art树的迭代器
+type artIterator struct {
+}
