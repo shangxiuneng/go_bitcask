96753e54a52346d22a349f7346a4d1b3d3c1062f
shangxiuneng <shangxiuneng95@163.com>
2024-10-21 21:36:12 +0800

    feat: art tree实现 单元测试完成

--- index/art.go
+++ index/art.go
@@ -50,9 +50,82 @@ func (a *ArtTree) Delete(key []byte) error {
 }
 
 func (a *ArtTree) Iterator(reverse bool) Iterator {
-	return nil
+	return newArtIterator(a.artTree, reverse)
 }
 
 // art树的迭代器
 type artIterator struct {
+	Iterator
+
+	currIndex int
+	reverse   bool    // 是否为反向遍历
+	values    []*Item // key位置对应的value
+}
+
+func newArtIterator(tree art.Tree, reverse bool) Iterator {
+
+	var idx int
+	if reverse {
+		idx = tree.Size() - 1
+	}
+	values := make([]*Item, tree.Size())
+	saveValues := func(node art.Node) bool {
+		item := &Item{
+			key:    node.Key(),
+			record: node.Value().(*data.RecordPos),
+		}
+		values[idx] = item
+		if reverse {
+			idx--
+		} else {
+			idx++
+		}
+		return true
+	}
+
+	tree.ForEach(saveValues)
+
+	return &artIterator{
+		currIndex: 0,
+		reverse:   reverse,
+		values:    values,
+	}
+
+	return &artIterator{
+		reverse: reverse,
+	}
+}
+
+// Rewind 重新回到迭代器的起点
+func (a *artIterator) Rewind() {
+	a.currIndex = 0
+}
+
+// Seek 根据传入的key，查找到第一个大于或小于目标的key 从这个key开始遍历
+func (a *artIterator) Seek(key []byte) {
+	// TODO
+	panic("Seek")
+}
+
+func (a *artIterator) Next() {
+	a.currIndex++
+}
+
+// Valid 当前迭代器是否有效
+func (a *artIterator) Valid() bool {
+	return a.currIndex < len(a.values)
+}
+
+// Key 当前迭代器指向的key数据
+func (a *artIterator) Key() []byte {
+	return a.values[a.currIndex].key
+}
+
+func (a *artIterator) Value() *data.RecordPos {
+	return a.values[a.currIndex].record
+}
+
+// Close 关闭迭代器 释放相应的资源
+func (a *artIterator) Close() {
+	a.currIndex = len(a.values) + 1
 }
