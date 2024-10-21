b716f3ba38fa05f84c1bbe6fe57e3bed7ba1f786
shangxiuneng <shangxiuneng95@163.com>
2024-10-21 21:00:10 +0800

    feat: 增加对redis hash和list的单元测试

--- redis/hash_test.go
+++ redis/hash_test.go
@@ -18,9 +18,13 @@ func TestService_HGet_HSet(t *testing.T) {
 	ok, err := redis.HSet([]byte("key"), []byte("filed1"), []byte("value1"))
 	assert.Nil(t, err)
 	assert.True(t, ok)
-	ok, err = redis.HSet([]byte("key"), []byte("filed12"), []byte("value12"))
+	ok, err = redis.HSet([]byte("key"), []byte("filed2"), []byte("value2"))
 	assert.Nil(t, err)
 	assert.True(t, ok)
+
+	value, err := redis.HGet([]byte("key"), []byte("filed1"))
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("value1"), value)
 }
 
 func TestService_HDel(t *testing.T) {
---
+++ redis/list_test.go
@@ -0,0 +1,7 @@
+package redis
+
+import "testing"
+
+func TestService_LPush(t *testing.T) {
+
+}
